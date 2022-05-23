e9783b94bfbaca6c91fbeef3a46f83fbb335c8a1
eslam salem <50119388+Eslam-Salem@users.noreply.github.com>
2022-05-23 19:47:21 +0200

    remove space in the package name

--- Tests/MBRadioButton Tests/MBRadioButton_Tests.swift
+++ Tests/MBRadioButton Tests/MBRadioButton_Tests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import MBRadioButton_
+@testable import MBRadioButton
 
 final class MBRadioButton_Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MBRadioButton_().text, "Hello, World!")
-    }
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(MBRadioButton.text, "Hello, World!")
+//    }
 }
